9ed7bf57766ce3a89d3ac6bad3329e69479bd007
Steven Shweky <s.shweky@petspeople.com>
2026-05-17 13:38:10 -0400

    Auto-sync 2026-05-17 13:38:09

--- skills/inventory-forecaster-cc/codepage/viewer.js
+++ skills/inventory-forecaster-cc/codepage/viewer.js
@@ -2224,7 +2224,7 @@ function toggleDetail(key) {
           </table>
         </div>
         ${gapBannerHtml}
-        <div id="inv-cards-${safeId}" style="margin-top:6px;"></div>
+        <div id="inv-cards-${safeIdForTotal}" style="margin-top:6px;"></div>
       </div>`;
   }
 
